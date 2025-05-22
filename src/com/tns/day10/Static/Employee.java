c88cb9ba480ed99c275b8d5dc0ec13bf7df54ad6
sprakash2004 <sathiyaprakash2004@gmail.com>
2025-05-22 11:50:01 +0530

    Added new files

---
+++ src/com/tns/day10/Static/Employee.java
@@ -0,0 +1,5 @@
+package com.tns.day10.Static;
+
+public class Employee {
+
+}
---
+++ src/com/tns/day10/Static/EmployeeDemo.java
@@ -0,0 +1,10 @@
+package com.tns.day10.Static;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
